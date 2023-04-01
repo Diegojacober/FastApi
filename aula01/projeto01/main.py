4ab26e3339d3952ffa47de26ccb795476b91278e
Diego <diegoalencar.jacober@gmail.ocm>
2023-04-01 13:29:30 -0300

    Injeção de dependencias

--- aula01/projeto01/main.py
+++ aula01/projeto01/main.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import List, Optional, Any
 
 from fastapi import FastAPI
 from fastapi import HTTPException
@@ -8,8 +8,21 @@ from fastapi import Response
 from fastapi import Path
 from fastapi import Query
 from fastapi import Header
+from fastapi import Depends
+
+
+from time import sleep
 
 from models import Curso
+
+def fake_db():
+    try:
+        print('Abindo conexão com o banco de dados')
+        sleep(1)
+    finally:
+        print('Fechando conexão com o banco de dados')
+        sleep(1)
+
 app = FastAPI()
 
 
@@ -27,13 +40,13 @@ cursos = {
 }
 
 @app.get("/cursos")
-async def get_cursos():
+async def get_cursos(db: Any = Depends(fake_db)):
     return cursos
 
 @app.get("/cursos/{curso_id}")
 # gt -> deve ser maior que 
 #lt deve ser menor que
-async def get_curso(curso_id : int = Path(title="ID do curso", description="Deve ser entre 1 e 2",gt=0, lt=3)):
+async def get_curso(curso_id : int = Path(title="ID do curso", description="Deve ser entre 1 e 2",gt=0, lt=3), db: Any = Depends(fake_db)):
     try:
         curso = cursos[curso_id]
         # coloca uma nova chave chamada id e coloca o id
@@ -54,7 +67,7 @@ async def calcular(a: int = Query(default=None, gt=5, lt=150), b: int = Query(de
 
 
 @app.post("/cursos", status_code=status.HTTP_201_CREATED)
-async def post_curso(curso: Curso): #Optional[Curso] = None):
+async def post_curso(curso: Curso, db: Any = Depends(fake_db)): #Optional[Curso] = None):
     next_id = len(cursos) + 1
     # curso.id = next_id
     cursos[next_id] = curso
@@ -63,7 +76,7 @@ async def post_curso(curso: Curso): #Optional[Curso] = None):
 
 
 @app.put('/cursos/{curso_id}')
-async def put_curso(curso_id: int, curso: Curso):
+async def put_curso(curso_id: int, curso: Curso, db: Any = Depends(fake_db)):
     if curso_id in cursos:
         cursos[curso_id] = curso
         curso.id = curso_id
@@ -72,7 +85,7 @@ async def put_curso(curso_id: int, curso: Curso):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Não existe o curso com o id {curso_id}")
 
 @app.delete('/cursos/{curso_id}')
-async def curso_delete(curso_id: int):
+async def curso_delete(curso_id: int, db: Any = Depends(fake_db)):
     if curso_id in cursos:
         del cursos[curso_id]
         # return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content="Deletado com sucesso")
@@ -84,4 +97,4 @@ async def curso_delete(curso_id: int):
         
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run("main:app",host='127.0.0.1', port=8000, log_level="info", reload=True) 
+    uvicorn.run("main:app",host='192.168.0.89', port=8000, log_level="info", reload=True) 
