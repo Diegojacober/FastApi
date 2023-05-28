f90b14e91d60b4764b3064b7d4ad25c77ae495b3
Diego <diegoalencar.jacober@gmail.ocm>
2023-05-28 18:30:38 -0300

    Criando models

---
+++ secao06/models/artigo_model.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, ForeignKey, String
+from sqlalchemy.orm import relationship
+
+from core.configs import settings
+
+class ArtigoModel(settings.DB_BASEMODEL):
+    __tablename__ = 'artigos'
+    
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    titulo = Column(String(256))
+    url_font: str = Column(String(String(256)))
+    usuario_id = Column(Integer, ForeignKey('usuarios.id'))
+    criador = relationship("UserModel",
+                           back_populates='artigos',
+                           lazy='joined')
--- secao06/models/usuario_model.py
+++ secao06/models/usuario_model.py
@@ -1,4 +1,22 @@
+from sqlalchemy import Column, Integer, ForeignKey, String, Boolean
+from sqlalchemy.orm import relationship
 
+from core.configs import settings
 
-class UserModel:
-    ...
+class UserModel(settings.DB_BASEMODEL):
+    __tablename__ = 'usuarios'
+    
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nome = Column(String(256), nullable=True)
+    sobrenome = Column(String(256), nullable=True)
+    email = Column(String(256), index=True, nullable=False, unique=True)
+    senha = Column(String(256), nullable=False)
+    is_admin = Column(Boolean, default=False)
+    artigos = relationship(
+        "ArtigoModel",
+        cascade='all, delete-orphan',
+        back_populates="criador",
+        uselist=True,
+        lazy="joined"
+    )
+    
