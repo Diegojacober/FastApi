fb0108ca90a1092733612b879863a0174b380173
Diego <diegoalencar.jacober@gmail.ocm>
2023-05-28 19:29:45 -0300

    Rotas de artigos

--- secao06/schemas/artigo_schema.py
+++ secao06/schemas/artigo_schema.py
@@ -4,9 +4,9 @@ from pydantic import BaseModel, HttpUrl
 
 class ArtigoSchema(BaseModel):
     id: Optional[int] = None
-    titulo: str
-    descricao: str
-    url_fonte: HttpUrl
+    titulo: Optional[str]
+    descricao: Optional[str]
+    url_fonte: Optional[HttpUrl]
     usuario_id: Optional[int]
     
     
